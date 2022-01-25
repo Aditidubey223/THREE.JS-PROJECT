af51cec688ff7ee9586d32493afe6b9498799e1f
aditi dubey <84570668+Aditidubey223@users.noreply.github.com>
2022-01-25 17:07:33 +0530

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import * as THREE from 'three';
+import {CSS3DRenderer , CSS3DObject} from 'three/examples/jsm/renderers/CSS2DRenderer'
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader"
+import {OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
